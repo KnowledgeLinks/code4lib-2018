855d499258db9d41c98ed4d677b08852bc365a55
Jeremy Nelson <jermnelson@gmail.com>
2018-02-13 14:25:08 -0500

    added class session and content changes

---
+++ freeze.py
@@ -0,0 +1,12 @@
+__author__ = "Jeremy Nelson"
+from flask_frozen import Freezer
+from app import app
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["FREEZER_RELATIVE_URLS"] = True
+app.config['FREEZER_BASE_URL'] = 'http://knowledgelinks.io/'
+
+freezer = Freezer(app)
+
+if __name__ == '__main__':
+    freezer.freeze()
